c36843ca4cfb106dd46718eb4c663a50dbf290ff
Spokay <hugobressange@gmail.com>
2023-10-19 22:55:06 +0200

    gitignore updated

--- lib/config/auth_api_config.dart
+++
@@ -1,6 +0,0 @@
-class AuthApiConfig{
-  static const String authUrl = "/api/auth";
-  static const String apiHost = "10.0.2.2";
-  static const int apiPort = 8080;
-  static const String apiUrl = "$apiHost:$apiPort$authUrl";
-}
--- lib/config/weather_api_config.dart
+++
@@ -1,7 +0,0 @@
-class WeatherApiConfig{
-  static const String weatherUrl = "/v1";
-  static const String apiHost = "api.weatherapi.com";
-  static const int apiPort = 443;
-  static const String apiUrl = "$apiHost$weatherUrl";
-  static const String apiKey = "9edae77d9be348e68cc155212231210";
-}
